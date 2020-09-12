84051dc39d9b8df6ee809283847a6d0c87b8d234
Mohit Singh <mohitsingh.shiv@gmail.com>
2020-09-13 01:04:52 +0530

    feat: add everwebinar api

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const axios = require("axios");
 
 const app = express();
 const bodyparser = require("body-parser");
@@ -8,7 +9,18 @@ const port = process.env.PORT || 3200;
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: false }));
 
-app.post("/demo", (req, res) => {
+app.post("/demo", async (req, res) => {
+  var responce = await axios
+    .post("https://api.webinarjam.com/everwebinar/webinars", {
+      api_key: "917c751f-d178-4960-ba44-04a105f1dd10",
+    })
+    .then((responce) => {
+      return responce;
+    })
+    .catch((err) => {
+      console.log("err", err);
+    });
+  console.log(":::>", JSON.stringify(responce.data));
   res.status(200).json({
     Data: req.body,
   });
